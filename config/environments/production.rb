28f72c7620714dccceb41a5bfc3f6378bc7d5641
xdite <xuite.joke@gmail.com>
2016-02-15 15:46:27 +0800

    set ruby version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,10 @@ Rails.application.configure do
   # Use a different cache store in production.
   config.cache_store = :dalli_store
 
+  config.assets.configure {|env|
+    env.cache = ActiveSupport::Cache.lookup_store(:dalli_store)
+  }
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 

dc77e843f269ab7a110e36ce2fcad2adcde5e6bf
xdite <xuite.joke@gmail.com>
2016-02-16 23:08:09 +0800

    Add form

---
+++ app/controllers/todo_comments_controller.rb
@@ -0,0 +1,2 @@
+class TodoCommentsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,10 @@ Rails.application.routes.draw do
       end
     end
 
+    resources :todos do
+      resources :comments
+    end
+
     resources :media_contents, only: [:create]
 
     resources :invitations do
